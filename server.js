961f0be52fddc4f66494fc99b18e546b380a4d24
Sayam Ajmal <itssanesayam@gmail.com>
2022-06-22 11:59:32 +0100

    created request handlers for the workoutRoute

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
+
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 
 // express app
@@ -11,9 +13,12 @@ app.use((req,res,next) => {
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-  })
+app.use('/api/workouts',workoutRoutes)
+
+
+// app.get('/', (req, res) => {
+//     res.json({mssg: 'Welcome to the app'})
+//   })
 
 //listen for request
 app.listen(process.env.PORT, () => {  
