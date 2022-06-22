eeea18fe3c998e9274999db66f2db64fc5f6f232
Sayam Ajmal <itssanesayam@gmail.com>
2022-06-22 12:09:37 +0100

    created another middleware 4 sending data to serve

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const workoutRoutes = require('./routes/workouts')
 // express app
 const app = express()
 
+//middleware
+app.use(express.json())
+
 app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()
